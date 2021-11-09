a851ec5adf409f9de0bcdd04074b1db2ff90198f
Paulo Henrique <pauloh8755@gmail.com>
2021-11-09 08:07:00 -0300

    busca por video concluída

--- js/scriptApi.js
+++ js/scriptApi.js
@@ -1,43 +1,66 @@
 "use stritic";
+
+//function para limpar elementos ao pesquisar
 const limparElementos = elemento =>{
     while(elemento.firstChild){
         elemento.removeChild(elemento.lastChild);
     }
 }
-
-const carregarStatus = (status,pesquisa) =>{
+//function para carregar o status de busca
+const carregarStatus = (status,pesquisa,tipoBusca) =>{
     const container = document.querySelector(".status");
     const newStatus = document.createElement("p");
     newStatus.classList = ".txt-padrao";
-    newStatus.innerHTML = `${status.totalHits} fotos grátis para ${pesquisa}`;
+    newStatus.innerHTML = `${status.totalHits} ${tipoBusca} grátis para ${pesquisa}`;
     container.appendChild(newStatus);
 }
-
+//function para retornar infromações sobre a busca(tipo, categoria, e pasta)
+const tipoBusca = ()=>{
+    //recebendo value do select
+    const busca = document.querySelector(".categorias").value;
+    //extraindo tipo de busca (image ou video)
+    let tipoBusca = busca.substring(0,busca.lastIndexOf('.'));
+    //extraindo categoria
+    const categorias = busca.substring(busca.lastIndexOf('.') + 1);    
+        
+    //testando busca por video ou imagem
+    if(tipoBusca == "imagens"){
+        var urlBusca = `image_type=${categorias}`;
+        var pastaVideo = "";
+    }
+    else{
+        var urlBusca = `video_type=${categorias}`;
+        var pastaVideo = "videos/";
+    }
+    const arrayBusca = [urlBusca,pastaVideo,tipoBusca];
+    return arrayBusca;
+}
+//function para pesquisar imagens 
 const pesquisarImagens = async(evento) =>{
     if(evento.key == "Enter"){
-        //recebendo value do select
-        const tipoImagem = document.querySelector(".categorias").value;
+        //recebendo tipo de busca (video ou image) e dirretório video
+        const busca = tipoBusca();
         //recebendo value do input
         const pesquisa = evento.target.value;
         //url json pixabay
-        const url = `https://pixabay.com/api/?key=23670717-85b5103b3d880933d4e67c566&q=${pesquisa}&image_type=${tipoImagem}`;
+        const url = `https://pixabay.com/api/${busca[1]}?key=23670717-85b5103b3d880933d4e67c566&q=${pesquisa}&lang=pt&${busca[0]}`;
         //fazendo requisição
         const response = await fetch(url);
         //Extraindo json
         const imagens = await response.json();
-        console.log(imagens);
 
         limparElementos(document.querySelector("#container-galeria"));
         limparElementos(document.querySelector(".status"));
 
         carregarGaleria(imagens.hits);
-        carregarStatus(imagens,pesquisa);
+        carregarStatus(imagens,pesquisa,busca[2]);
     }
 }
+//function para criar cards de imagens
 const criarItem = item =>{
     const container = document.querySelector("#container-galeria");
     const newCard =document.createElement("div");
-//    newCard.href = `${item.pageURL}`;
+    //newCard.href = `${item.pageURL}`;
     const tags = item.tags.replace(/,+/g, '');
     newCard.innerHTML = `
                 <a class="img-perfil" href="https://pixabay.com/users/${item.user}-${item.user_id}/">
@@ -52,9 +75,8 @@ const criarItem = item =>{
                     </div>
                 </div>
                 <a class="card-image"href="${item.pageURL}">
-                <img class="card-image" src="${item.webformatURL}">
+                <img class="card-image" src="${item.webformatURL?item.webformatURL:'https://i.vimeocdn.com/video/'+ item.picture_id+'_640x360.jpg'} ">
                 </a>
-            
             `;
     container.appendChild(newCard);
 }
